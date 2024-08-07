0cd97ffed24140c8faf5f79799b4e596c4591176
RodrigoTanchela <rmtanchela@gmail.com>
2024-08-07 17:36:02 -0300

    Heading component

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,7 +1,36 @@
 import styled, { css } from 'styled-components';
 
+const titleSize = {
+  small: (theme) => css`
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme) => css`
+    font-size: ${theme.font.sizes.small};
+  `,
+  big: (theme) => css`
+    font-size: ${theme.font.sizes.large};
+  `,
+  huge: (theme) => css`
+    font-size: ${theme.font.sizes.huge};
+    ${mediaFont(theme)};
+  `,
+};
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.large};
+    background: red;
+  }
+`;
+
+const titleCase = (uppercase) => css`
+  text-transform: ${uppercase ? 'uppercase' : 'none'};
+`;
+
 export const Title = styled.h1`
-  ${({ theme }) => css`
-    color: ${theme.colors.mainBg};
-  `}
+  ${({ theme, colorDark, size, uppercase }) => css`
+    color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
+    ${titleSize[size](theme)};
+    ${titleCase(uppercase)};
+  `};
 `;
--- src/templates/App/styles.js
+++ src/templates/App/styles.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-${({ background }) => css`
-   background: ${background}`}
+  ${({ background }) => css`
+    background: ${background};
+  `}
 `;
